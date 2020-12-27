1e271156ec6db45da9af0b80bc3e739db4f4bd71
Psp29onetwo <pspofficial29@gmail.com>
2020-12-27 17:55:56 +0530

    sciket-learn added

---
+++ jupyter.bat
@@ -0,0 +1,1 @@
+%USERPROFILE%\Anaconda3\python.exe %USERPROFILE%\Anaconda3\cwp.py %USERPROFILE%\Anaconda3 %USERPROFILE%\Anaconda3\python.exe %USERPROFILE%\Anaconda3\Scripts\jupyter-lab-script.py %CD% SETCONSOLE /hide
